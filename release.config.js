57645377cbd53c07e88be7256021d919d5dcfacc
Marco Lovazzano <marco.lovazzano@gmail.com>
2022-04-09 22:10:47 +0200

    BREAKING CHANGE: change config file
    * Create release.config.js
    
    * Delete .releaserc.yml

---
+++ release.config.js
@@ -0,0 +1,44 @@
+// Ref:
+// - https://github.com/semantic-release/semantic-release
+// - https://semantic-release.gitbook.io/semantic-release/extending/plugins-list
+// - https://svdoscience.com/2020-10-31/versioning-with-semantic-release
+
+module.exports = {
+  'branches': [
+    'master',
+    {
+      name: 'develop',
+      prerelease: true
+    },
+    {
+      name: 'feat/*',
+      prerelease: true
+    }
+  ],
+  'preset': 'conventionalcommits',
+  'tagFormat': 'v${version}',
+  'plugins': [
+    '@semantic-release/commit-analyzer',
+    ['@semantic-release/release-notes-generator', {
+        preset: 'conventionalcommits',
+        "writerOpts": {
+          "commitsSort": ["perf", "feat", "fix"],
+        }
+      }
+    ],
+    ['@semantic-release/changelog', {
+        "changelogFile": "CHANGELOG.md",
+      }
+    ],
+    ["@semantic-release/git", {
+        "assets": ["docs/CHANGELOG.md"]
+      }
+    ],
+    ["@semantic-release/github", {
+      "assets": [
+        {"path": "CHANGELOG.md", "label": "changelog"},
+        {"path": "dist/**", "label": "distribution"}
+      ]
+    }],
+  ]
+}
