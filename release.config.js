bc085d91466119e98ee816e63b5811f1cb385a8d
Marco Lovazzano <marco.lovazzano@gmail.com>
2022-04-10 20:13:03 +0200

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -21,7 +21,11 @@ module.exports = {
           "commitsSort": ["perf", "feat", "fix"],
         }
       }
-    ],    
+    ],
+    ["@semantic-release/exec", {
+        "verifyReleaseCmd": "echo ${nextRelease.version} > .VERSION"
+      }
+    ],
     ['@semantic-release/changelog', {
         "changelogFile": "CHANGELOG.md",
       }
