c2e477c40c0affaef1e853b3799c8bab63a99d5f
Marco Lovazzano <marco.lovazzano@gmail.com>
2022-05-07 08:40:27 +0200

    feat!: test

--- release.config.js
+++ release.config.js
@@ -3,47 +3,53 @@
 // - https://semantic-release.gitbook.io/semantic-release/extending/plugins-list
 // - https://svdoscience.com/2020-10-31/versioning-with-semantic-release
 
-module.exports = {
-  'branches': [
-    'master',
-    {
-      'name': 'develop',
-      'channel': 'channel-${name}',
-      'prerelease': true
-    },
-  ],
-  'repositoryUrl': 'https://github.com/martcus/try-semantic',
-  'tagFormat': 'v${version}',
-  'preset': 'conventionalcommits',
-  'plugins': [
-    ['@semantic-release/commit-analyzer', {
-      }
+const { execSync } = require('child_process');
+
+module.exports = getReleaseConfig();
+
+function getReleaseConfig() {
+  return {
+    'branches': [
+      'master',
+      {
+        'name': 'develop',
+        'channel': 'channel-${name}',
+        'prerelease': true
+      },
     ],
-    ['@semantic-release/release-notes-generator', {
-        'writerOpts': {
-          'commitsSort': ['perf', 'feat', 'fix'],
+    'repositoryUrl': 'https://github.com/martcus/try-semantic',
+    'tagFormat': 'v${version}',
+    'preset': 'conventionalcommits',
+    'plugins': [
+      ['@semantic-release/commit-analyzer', {
         }
-      }
-    ],
-    ['@semantic-release/exec', {
-        'verifyReleaseCmd': 'echo ${nextRelease.version} > .VERSION',
-        'prepareCmd': './prepare-release.sh ${nextRelease.version}'
-      }
-    ],
-    ['@semantic-release/changelog', {
-        'changelogFile': 'CHANGELOG.md',
-        'changelogTitle': '# Semantic Release Changelog'
-      }
-    ],
-    ['@semantic-release/git', {
-        'assets': ['CHANGELOG.md']
-      }
-    ],
-    ['@semantic-release/github', {
-      'assets': [
-        {'path': 'CHANGELOG.md'},
-        {'path': 'dist/**', 'label': 'distribution'}
-      ]
-    }],
-  ]
+      ],
+      ['@semantic-release/release-notes-generator', {
+          'writerOpts': {
+            'commitsSort': ['perf', 'feat', 'fix'],
+          }
+        }
+      ],
+      ['@semantic-release/exec', {
+          'verifyReleaseCmd': 'echo ${nextRelease.version} > .VERSION',
+          'prepareCmd': './prepare-release.sh ${nextRelease.version}'
+        }
+      ],
+      ['@semantic-release/changelog', {
+          'changelogFile': 'CHANGELOG.md',
+          'changelogTitle': '# Semantic Release Changelog'
+        }
+      ],
+      ['@semantic-release/git', {
+          'assets': ['CHANGELOG.md']
+        }
+      ],
+      ['@semantic-release/github', {
+        'assets': [
+          {'path': 'CHANGELOG.md'},
+          {'path': 'dist/**', 'label': 'distribution'}
+        ]
+      }],
+    ]
+  }
 }
