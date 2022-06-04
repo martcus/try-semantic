a16e4b4d33be20b6b3c439266e18ac2ca577642d
Marco Lovazzano <marco.lovazzano@gmail.com>
2022-06-04 11:24:31 +0200

    fix: test

--- release.config.js
+++ release.config.js
@@ -36,7 +36,8 @@ module.exports = {
       }
     ],
     ['@semantic-release/git', {
-        'assets': ['CHANGELOG.md']
+        'assets': ['CHANGELOG.md'],
+        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       }
     ],
     ['@semantic-release/github', {
