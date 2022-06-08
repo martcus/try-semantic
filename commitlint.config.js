083148c4e7d43d8b09b25c744608830227567a69
Marco Lovazzano <marco.lovazzano@gmail.com>
2022-06-08 07:49:56 +0200

    fix: The blinking lights of the antenna tower came into focus just as I heard a loud snap. Erin accidentally created a new universe.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,12 @@
 // https://commitlint.js.org/
 
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'header-max-length': [2, 'always', 200],
+  }
 };
